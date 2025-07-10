882bd08b91b1fb2ce973015a639f833209225f33
Oksana Vorobei <oks.vorobei@ukr.net>
2025-07-10 12:56:58 +0300

    add task

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function getElementWidth(content, padding, border) {
+  const numContent = Number.parseFloat(content);
+  const numPadding = Number.parseFloat(padding);
+  const numBorder = Number.parseFloat(border);
+
+  const fullPadding = numPadding * 2;
+  const fullBorderBox = numBorder * 2;
+
+  const sumTotalWidth = numContent + fullPadding + fullBorderBox;
+
+  return sumTotalWidth;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
